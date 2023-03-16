3868240ca353f6b85a61796bd5f55e7aed399204
Max <Max>
2023-03-16 23:38:52 +0300

    начал шаг 4

---
+++ src/even.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync'; 
+import greeting from "cli.js";
+const evengame = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    
+
+}; 
+
